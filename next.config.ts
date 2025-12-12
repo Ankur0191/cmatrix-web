d29ceffe6ee43fef864dfe6ad4e07dc513f3dcd2
Ankur0191 <ankurmishra11187@gmail.com>
2025-12-12 18:23:45 +0530

    config changes - build was failing

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable ESLint blocking production builds
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // Disable TypeScript errors during build (they still show in VSCode)
   typescript: {
     ignoreBuildErrors: true,
   },
-
-  experimental: {
-    typedRoutes: false, // Fixes Next.js 15 dynamic route type bug
-  },
-
   images: {
-    domains: ["cdn.sanity.io"],
+    domains: ['cdn.sanity.io'],
   },
 };
 
